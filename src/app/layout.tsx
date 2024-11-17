15e9da7197399f2608df9bbf0e7038bd2850957e
Silvia <silvia@puntodev.es>
2024-11-17 12:35:32 +0100

    added metadata and credits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,49 @@ import { Metadata } from "next/types";
 import App from "./app";
 
 export const metadata: Metadata = {
-  title: 'Rocco Modugno',
-  description: 'My App is a...',
+  title: "Rocco Modugno | Creative Graphic Designer",
+  description: "Discover Rocco Modugno’s portfolio, showcasing expertise in editorial design, visual communication, creative coding, and design consulting.",
+  applicationName: "Rocco Modugno Portfolio",
+  authors: [
+    {
+      name: "Rocco Modugno",
+      url: "https://rlmodugno.com"
+    },
+    {
+      name: "Silvia Stefani",
+      url: "https://silviastefani.com"
+    }
+  ],
+  openGraph: {
+    title: 'Rocco Modugno | Editorial Design & Visual Communication | rlmodugno',
+    description: 'Explore projects in editorial design, typography, visual communication, and creative coding by Rocco Modugno.',
+    url: 'https://rlmodugno.com',
+    siteName: "Rocco Modugno Portfolio",
+    images: [
+      {
+        url: 'https://rlmodugno.com/storie_d_arte_e_di_critica/sac_img_1.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Preview of Rocco Modugno’s portfolio',
+      },
+    ],
+    locale: "en_EN",
+    type: 'website',
+  },
+  viewport: 'width=device-width, initial-scale=1',
+  icons: {
+    icon: '/favicon.ico',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  metadataBase: new URL('https://rlmodugno.com'),
+  formatDetection: {
+    email: true,
+    address: false,
+    telephone: false,
+  },
 }
 
 export default function RootLayout({
@@ -21,7 +62,7 @@ export default function RootLayout({
   · Code: Silvia Stefani
   · Date: January 2024
   · Language: Typescript
-  · Tools: Vite & React
+  · Tools: Next & React
   `);
 
   return (
